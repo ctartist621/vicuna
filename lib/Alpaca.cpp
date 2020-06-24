863679b9bbaae00c2f3d0a5d6c3f7018c9765d20
David B Mittelman <dbmittelman@gmail.com>
2020-06-24 15:08:38 -0700

    Header files for Stock History and Trade classes

--- lib/Alpaca.cpp
+++ lib/Alpaca.cpp
@@ -1,11 +1,11 @@
 #include "Alpaca.h"
+
+#include <chrono>
+#include <ctime>
 #include <iostream>
 #include <string>
-#include <ctime>
-#include <chrono>
 
-std::string Alpaca::_parse_stream_message(alpaca::stream::DataType data)
-{
+std::string Alpaca::_parse_stream_message(alpaca::stream::DataType data) {
   rapidjson::Document d;
   d.Parse(data.c_str());
   rapidjson::StringBuffer s;
@@ -14,20 +14,16 @@ std::string Alpaca::_parse_stream_message(alpaca::stream::DataType data)
   return s.GetString();
 };
 
-std::vector<alpaca::Bar> Alpaca::MarketData_Bars(const std::string symbol)
-{
+std::vector<alpaca::Bar> Alpaca::MarketData_Bars(const std::string symbol, const std::string timeframe) {
   std::cout << "Retrieving bars for " << symbol << std::endl;
   auto client = alpaca::Client(_env);
 
-  auto bars_response = client.getBars({symbol}, "", "", "", "1Min");
+  auto bars_response = client.getBars({symbol}, "", "", "", timeframe);
 
-  if (auto status = bars_response.first; !status.ok())
-  {
+  if (auto status = bars_response.first; !status.ok()) {
     std::cerr << "Error getting bars information: " << status.getMessage() << std::endl;
     throw status.getCode();
-  }
-  else
-  {
+  } else {
     return bars_response.second.bars[symbol];
   }
 }
---
+++ lib/Trade.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "Alpaca.h"
+#include "StockHistory.h"
+
+#include <future>
+#include <iostream>
+#include <memory>
+#include <string>
+#include <thread>
+#include <vector>
+
+class Trade : public std::enable_shared_from_this<Trade> {
+ public:
+  Trade(std::shared_ptr<Alpaca> a, std::string s) : alpaca(a), symbol(s), stockHistory(a, s){};
+
+  ~Trade() {
+    // set up thread barrier before this object is destroyed
+    std::for_each(threads.begin(), threads.end(), [](std::thread& t) { t.join(); });
+  }
+
+  // miscellaneous
+  std::shared_ptr<Trade> get_shared_this() {
+    return shared_from_this();
+  }
+
+ private:
+  std::shared_ptr<Alpaca> alpaca;
+  std::string symbol;
+  StockHistory stockHistory;
+  std::vector<std::thread> threads;
+};
