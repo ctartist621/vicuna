ba84cbcd4cc13397e62ff275722925fc29ecac0a
David B Mittelman <dbmittelman@gmail.com>
2020-06-24 22:14:36 -0700

    setup corrections

--- setupWorkspace.sh
+++ setupWorkspace.sh
@@ -1,7 +1,13 @@
 sudo apt install -y curl gnupg
-curl https://bazel.build/bazel-release.pub.gpg | sudo apt-key add -
-echo "deb [arch=amd64] https://storage.googleapis.com/bazel-apt stable jdk1.8" | sudo tee /etc/apt/sources.list.d/bazel.list
 
-sudo apt update && sudo apt install -y bazel
-sudo apt update && sudo apt full-upgrade
-sudo apt install -y bazel-1.0.0
+sudo apt-get install -y openjdk-8-jdk
+
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get update -y && sudo apt-get install -y oracle-java8-installer
+
+echo "deb [arch=amd64] http://storage.googleapis.com/bazel-apt stable jdk1.8" | sudo tee /etc/apt/sources.list.d/bazel.list
+curl https://bazel.build/bazel-release.pub.gpg | sudo apt-key add -
+
+sudo apt-get update -y && sudo apt-get install -y bazel
+sudo apt-get upgrade -y bazel
+
