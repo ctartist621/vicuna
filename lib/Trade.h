22deda0c6153d6253b3c866a9583285f8ec7172e
David B Mittelman <dbmittelman@gmail.com>
2020-06-24 17:02:39 -0700

    CheckIndicators

--- lib/Trade.h
+++ lib/Trade.h
@@ -12,12 +12,16 @@
 
 class Trade : public std::enable_shared_from_this<Trade> {
  public:
-  Trade(std::shared_ptr<Alpaca> a, std::string s) : alpaca(a), symbol(s), stockHistory(a, s){};
+  Trade(std::shared_ptr<Alpaca> a, std::string s) : alpaca(a), symbol(s), stockHistory(a, s) {
+    threads.emplace_back(std::thread(&Trade::CheckIndicators, this));
+  };
 
   ~Trade() {
     // set up thread barrier before this object is destroyed
     std::for_each(threads.begin(), threads.end(), [](std::thread& t) { t.join(); });
   }
+
+  void CheckIndicators();
 
   // miscellaneous
   std::shared_ptr<Trade> get_shared_this() {
