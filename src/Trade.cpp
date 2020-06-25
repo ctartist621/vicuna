cb5c2b25d6dcaf1c800c67845064eef7c98d953f
David B Mittelman <dbmittelman@gmail.com>
2020-06-24 18:30:25 -0700

    Calculate SMA

--- src/Trade.cpp
+++ src/Trade.cpp
@@ -1,10 +1,15 @@
 #include "Trade.h"
-
 #include <chrono>
 #include <string>
 
-double SimpleMovingAverage(std::vector<alpaca::Bar>)
+double SimpleMovingAverage(std::vector<alpaca::Bar> bars)
 {
+  double sum = 0;
+  for (alpaca::Bar b : bars)
+  {
+    sum += b.close_price;
+  }
+  return sum / bars.size();
 }
 
 void Trade::CheckIndicators()
@@ -15,13 +20,14 @@ void Trade::CheckIndicators()
 
     if (history.size() == 0)
     {
-      // noop
+      std::cout << "History not retrieved for " << symbol << ", waiting..." << std::endl;
     }
     else
     {
       std::cout << "CheckIndicators " << symbol << std::endl;
       double sma = SimpleMovingAverage(history);
+      std::cout << "SMA: " << sma << std::endl;
     }
+    std::this_thread::sleep_for(std::chrono::minutes(1));
   }
-  std::this_thread::sleep_for(std::chrono::minutes(1));
 };
